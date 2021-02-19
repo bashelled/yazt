61d2f1226aa6c777d9101c01458f1a77efde2f1c
bashelled <senskeleton@aol.com>
2021-02-19 01:48:53 +0000

    Added Makefile, templates and Yazp

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
+	
 move_files:
 	@echo "==> Moving Yazt files..."
 	mv -v ./* ~/.yazt
 
 copy_rc: move_files
-	@echo "==> Copying zshrc...
-	if [[ $ZSH == $HOME/.oh-my-zsh ]]; then
-	cat ./etc/templates/rcs/omz-enabled > $HOME/.zshrc
-	else
-        cat ./etc/templates/rcs/normal
+	@echo "==> Copying zshrc..."
+	cat ./etc/templates/rcs/normal >> ~/.zshrc
 chsh: move_files copy_rc
 	@echo "==> Changing shell..."
 	chsh -s $(which zsh)
+
 done: move_files copy_rc chsh
 	@echo "Yazt installed"
--- yazt.sh
+++ yazt.sh
@@ -5,6 +5,7 @@ if [[ -z "$yaztcustom" ]]; then
     yaztcustom="$yazt/usr"
 fi
 
+# Loads theme
 if [ ! "$yazttheme" = ""  ]; then
   if [ -f "$yaztcustom/$yazttheme.yzt" ]; then
     source "$yaztcustom/$yazttheme.yzt"
@@ -13,4 +14,21 @@ if [ ! "$yazttheme" = ""  ]; then
   else
     source "$yazt/themes/$yazttheme.yzt"
   fi
+fi
+
+# Yazp loader.
+if [ "$yazpon" = "true" ]; then
+for yazpplugin ($yazpplugins); do
+ if [ ! "$yazpplugin" = ""  ]; then
+  if [ -f "$yaztcustom/$yazpplugin.yzp" ]; then
+    source "$yaztcustom/$yazpplugin.yzp"
+  elif [ -f "$yaztcustom/plugins/$yazpplugin.yzp" ]; then
+    source "$yaztcustom/plugins/$yazpplugin.yzp"
+  else
+    source "$yazt/plugins/$yazpplugin.yzp"
+  fi
+ else
+   echo "yazp: no plugin specified"
+fi
+done
 fi
