77f0127d45b442e8d69cec10a31bce6f07946835
bashelled <senskeleton@aol.com>
2021-02-19 05:28:23 +0000

    Added Brew

--- tools/install.sh
+++
@@ -1,9 +0,0 @@
-echo "Installing Yazt..."
-sleep 1
-echo "==> Moving Yazt files..."
-git clone https://github.com/bashelled/yazt ~/.yazt
-echo "==> Copying zshrc..."
-cp ~/.yazt/etc/templates/rcs/normal  ~/.zshrc
-echo "==> Changing shell..."
-chsh -s $(which zsh)
-echo "Yazt installed"
